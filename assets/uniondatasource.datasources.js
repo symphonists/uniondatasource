8c64a0805758baebb8b0c141f775c1cf5e53ee12
Brendan Abbott <brendan@bloodbone.ws>
2012-02-04 09:06:22 +1000

    Fix logic to determine Sort By UI. RE: #8

--- assets/uniondatasource.datasources.js
+++ assets/uniondatasource.datasources.js
@@ -14,43 +14,51 @@ jQuery(document).ready(function() {
 			$duplicator.find('li:first').each(function() {
 				var $self = $(this),
 					sort = $self.find('input[name*=union-sort]').val(),
-					order = $self.find('input[name*=union-order]').val();
+					$existing_sort = $sort.find('option[value=' + sort + ']'),
+					order = $self.find('input[name*=union-order]').val()
+					$existing_order = $order.find('option[value=' + order + ']');
 
-				// Sort
-				var $option = $('<option />')
-								.attr('selected', 'selected')
-								.val(sort)
-								.text(sort);
+				// If this sort field already exists, select it
+				if($existing_sort.length) {
+					$existing_sort.attr('selected', 'selected');
+				}
+				// Otherwise add a new option and select it
+				else {
+					$sort.append(
+						$('<option />').attr('selected', 'selected').val(sort).text(sort)
+					);
+				}
 
-				$sort.append($option);
-
-				// Order
-				$order.append(
-					$option.clone().val(order).text(order)
-				);
-
+				// If this order already exists, select it
+				if($existing_order.length) {
+					$existing_order.attr('selected', 'selected');
+				}
+				// Otherwise add a new option and select it
+				else {
+					$order.append(
+						$('<option />').attr('selected', 'selected').val(order).text(order)
+					);
+				}
 			});
 		}
 
 		// Trigger inital Sort By/Order population
 		update();
 
-		// Listen for when the duplicator changes
-		// Update the sort options
-		$duplicator.bind('orderstop.duplicator click.duplicator', function() {
-			update();
-		});
+		$duplicator
+			// Listen for when the duplicator changes
+			// Update the sort options
+			.bind('orderstop.duplicator click.duplicator', update)
 
-		// When removing a duplicator item
-		$duplicator.bind('destruct.duplicator', function() {
-			if($duplicator.find('li').length) {
-				update();
-			}
-			else {
-				$sort.find('option').remove();
-				$order.find('option').remove();
-			}
-		});
+			// When removing a duplicator item
+			.bind('destruct.duplicator', function() {
+				if($duplicator.find('li').length) {
+					update();
+				}
+				else {
+					$sort.add($order).find('option').remove();
+				}
+			});
 
 	})(jQuery.noConflict());
 });
